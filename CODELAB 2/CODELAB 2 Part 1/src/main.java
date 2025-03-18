a23aeb95bdf930f4e15dd63a4b39272cb00cc9af
DikhaKrisnanda <dikhakrisnanda@gmail.com>
2025-03-18 23:11:02 +0700

    update terbaru

---
+++ CODELAB 2/CODELAB 2 Part 1/src/main.java
@@ -0,0 +1,41 @@
+// Kelas untuk Hewan
+class Hewan {
+    String nama;
+    String jenis;
+    String suara;
+
+    // bagian method
+    public Hewan(String nama, String jenis, String suara) {
+        this.nama = nama;
+        this.jenis = jenis;
+        this.suara = suara;
+    }
+
+    // Metode untuk menyapa
+    public void sapaan() {
+        System.out.println("Halo! Aku adalah seekor " + nama + " >.<");
+    }
+
+    // Metode untuk menampilkan informasi hewan
+    public void tampilkanInfo() {
+        sapaan();
+        System.out.println("---------------------------");
+        System.out.println("Nama  : " + nama);
+        System.out.println("Jenis : " + jenis);
+        System.out.println("Suara : " + suara);
+        System.out.println("---------------------------\n");
+    }
+}
+
+
+public class main {
+    public static void main(String[] args) {
+        // Membuat objek hewan
+        Hewan hewan1 = new Hewan("Kucing", "Mamalia", "Nyann~~");
+        Hewan hewan2 = new Hewan("Anjing", "Mamalia", "Woof-Woof!!");
+
+        // Menampilkan informasi hewan
+        hewan1.tampilkanInfo();
+        hewan2.tampilkanInfo();
+    }
+}
